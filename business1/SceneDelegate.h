a5eb8e1d538d06d7f8d315a677393333a638c719
gewei0701 <gewei0701@autohome.com.cn>
2021-01-15 15:51:56 +0800

    * 添加初始化文件

---
+++ business1/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  business1
+//
+//  Created by gewei on 2021/1/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
